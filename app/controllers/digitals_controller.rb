2d301da89c3ff4b5291fc2b69244ba845a44e642
Sam Davies <sam.jc.davies@gmail.com>
2011-07-05 21:20:57 +0100

    Adding S3 support

--- app/controllers/digitals_controller.rb
+++ app/controllers/digitals_controller.rb
@@ -6,8 +6,8 @@ class DigitalsController < Spree::BaseController
     link = DigitalLink.find_by_secret(params[:secret])
     if link.present? and link.digital.attachment.present?
       attachment = link.digital.attachment
-      if link.authorize! and File.file?(attachment.path)
-        send_file attachment.path :filename => attachment.original_filename, :type => attachment.content_type and return
+      if link.authorize!
+        redirect_to attachment.authenticated_url
       end
     end
     render :unauthorized
