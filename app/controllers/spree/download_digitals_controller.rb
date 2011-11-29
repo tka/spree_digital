63aa9aeb2c93d24a2f05531bf0df6481b1723683
tka <tkatka@gmail.com>
2011-11-29 17:22:36 +0800

    fix namespace

--- app/controllers/spree/download_digitals_controller.rb
+++ app/controllers/spree/download_digitals_controller.rb
@@ -3,7 +3,7 @@ class Spree::DownloadDigitalsController < Spree::BaseController
   ssl_required :show
 
   def show
-    link = DigitalLink.find_by_secret(params[:secret])
+    link = Spree::DigitalLink.find_by_secret(params[:secret])
     if link.present? and link.digital.attachment.present?
       if link.authorize!
         redirect_to link.digital.authenticated_url
